8c7696a42f6ec4eca84ceaec048b6f9e140cfc95
Mishari Muqbil <mishari@mishari.net>
2013-09-12 19:37:55 +0700

    added licensing information

--- test/test-url-mod.js
+++ test/test-url-mod.js
@@ -1,3 +1,21 @@
+/*
+    https-everywhere-helper
+    Copyright (C) 2013  Mishari A. Muqbil mishari@mishari.net
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see [http://www.gnu.org/licenses/].
+*/
+
 var urlmod = require("./url-mod");
  
 exports["test http2https"] = function(assert) {
